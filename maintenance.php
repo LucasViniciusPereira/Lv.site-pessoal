5924d9c6f90dd3d1e2594f1bd4ea3777233b562e
Lucas Vinicius <lv.lucasvin@gmail.com>
2016-01-24 00:06:00 -0200

    Refatoração do meu site pessoal

---
+++ maintenance.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8">
+<title>Página em Manutenção</title>
+</head>
+<body style="background:#FCFCFC">
+	<center>
+		<h1>Site em manutenção... </h1>
+		<h3>
+		Estamos em manutenção para melhorar nosso serviço</br>
+		Tente novamente mais tarde.
+		</h3>
+	</center>
+</body>
+</html>
